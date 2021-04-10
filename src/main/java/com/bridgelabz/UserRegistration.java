ad1b8299876e6a751bdaf2d6acb82256b5307d13
joshivinit2018 <joshivinit2018@gmailcom>
2021-04-10 09:36:52 +0530

    [vinit]:add email

--- src/main/java/com/bridgelabz/UserRegistration.java
+++ src/main/java/com/bridgelabz/UserRegistration.java
@@ -4,8 +4,23 @@ import java.util.regex.Pattern;
 
 public class UserRegistration {
     private static final String NAME_PATTERN = "^[A-Z]{1}[a-z]{2,}$";
-    public boolean validateFirstName(String FirstName) {
+
+    private static final String EMAIL_PATTERN = "^([a-zA-Z0-9_.$*&!+-]+)@([a-z0-9]+).([a-z.]{2,7})$";
+
+    public static boolean validateFirstName(String FirstName) {
         Pattern pattern = Pattern.compile(NAME_PATTERN);
         return pattern.matcher(FirstName).matches();
     }
+
+    public boolean validateLastName(String LastName) {
+        Pattern pattern = Pattern.compile(NAME_PATTERN);
+        return pattern.matcher(LastName).matches();
+    }
+
+    public boolean validateEmail(String Email) {
+        Pattern pattern = Pattern.compile(NAME_PATTERN);
+        return pattern.matcher(EMAIL_PATTERN).matches();
+    }
+
+
 }
--- src/test/java/com/bridgelabz/UserRegistrationtest.java
+++ src/test/java/com/bridgelabz/UserRegistrationtest.java
@@ -12,9 +12,37 @@ public class UserRegistrationtest {
     }
 
     @Test
-    void givenFirstName_WhenShort_ShouldReturnTrue() {
+    void givenFirstName_WhenShort_ShouldReturnFalse() {
         UserRegistration user = new UserRegistration();
-        boolean result = user.validateFirstName("ma");
+        boolean result = user.validateFirstName("vin");
+        Assertions.assertFalse(result);
+    }
+
+    @Test
+    void givenLastName_WhenShort_ShouldReturnTrue() {
+        UserRegistration user = new UserRegistration();
+        boolean result = user.validateLastName("Joshi");
+        Assertions.assertTrue(result);
+    }
+
+    @Test
+    void givenLastName_WhenShort_ShouldReturnFalse() {
+        UserRegistration user = new UserRegistration();
+        boolean result = user.validateLastName("Joshi");
+        Assertions.assertFalse(result);
+    }
+
+    @Test
+    void givenEmail_WhenShort_ShouldReturnTrue() {
+        UserRegistration user = new UserRegistration();
+        boolean result = user.validateEmail("JoshiVinit2014@gmsil.com");
+        Assertions.assertTrue(result);
+    }
+
+    @Test
+    void givenEmail_WhenShort_ShouldReturnFalse() {
+        UserRegistration user = new UserRegistration();
+        boolean result = user.validateEmail("Josh014gmsil.com");
         Assertions.assertFalse(result);
     }
 }
